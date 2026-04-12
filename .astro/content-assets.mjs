663c5731cf32005c000ad01913fe21729ca1bce6
Wesley Deland <wesdeland@gmail.com>
2026-04-12 14:57:45 -0500

    remove base tag

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
-import __ASTRO_IMAGE_IMPORT_ZDwp9K from "./opencode-screenshot.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fopencode.md";
 import __ASTRO_IMAGE_IMPORT_Z28vuFf from "./sleep-modes.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwindows-modern-standby.md";
 import __ASTRO_IMAGE_IMPORT_2bbE5y from "./hibernate-settings.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwindows-modern-standby.md";
-export default new Map([["./opencode-screenshot.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fopencode.md", __ASTRO_IMAGE_IMPORT_ZDwp9K], ["./sleep-modes.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwindows-modern-standby.md", __ASTRO_IMAGE_IMPORT_Z28vuFf], ["./hibernate-settings.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwindows-modern-standby.md", __ASTRO_IMAGE_IMPORT_2bbE5y]]);
+import __ASTRO_IMAGE_IMPORT_ZDwp9K from "./opencode-screenshot.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fopencode.md";
+export default new Map([["./sleep-modes.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwindows-modern-standby.md", __ASTRO_IMAGE_IMPORT_Z28vuFf], ["./hibernate-settings.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwindows-modern-standby.md", __ASTRO_IMAGE_IMPORT_2bbE5y], ["./opencode-screenshot.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fopencode.md", __ASTRO_IMAGE_IMPORT_ZDwp9K]]);
 		
